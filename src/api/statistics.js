1b809345fe5e2b2ce3ab454e01d38dca528bd413
717515614mc-gif <717515614mc@gmail.com>
2026-02-03 20:30:16 +0800

    Update statistics.js

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'http://demo01-222374-8-1400573097.sh.run.tcloudbase.com/api',
   timeout: 10000
 })
 
